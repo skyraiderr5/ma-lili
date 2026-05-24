268b8b9f8091a15e7eb21bbf4c02322b68e575f3
skyraiderr5 <130313880+skyraiderr5@users.noreply.github.com>
2026-05-24 11:47:50 -0400

    added so it waits on the terminal instead of immediately exiting

--- cur_convert.py
+++ cur_convert.py
@@ -10,3 +10,5 @@ if setting == 2:
     num2 = int(input("Type amount of kala lili: "))
     
     print(f"{num1} kala suli and {num2} kala lili is {round((num1 + num2/20) * 2.5, 2):.2f}$ CAD")
+
+input()
